bbf535d48af2f17b1584bc999a477ba1207b9694
JayAtSeneca <jpchaudhari@myseneca.ca>
2023-04-25 19:13:37 -0400

    Added the dotenv config in index.ts and update package script

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import dotenv from 'dotenv';
+dotenv.config();
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
@@ -47,6 +49,6 @@ const server = new ApolloServer({
 //  2. installs your ApolloServer instance as middleware
 //  3. prepares your app to handle incoming requests
 const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
+    listen: { port: process.env.PORT ? parseInt(process.env.PORT) : 4000 },
 });
 console.log(`🚀  Server ready at: ${url}`);
