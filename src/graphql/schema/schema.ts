65be82e692bf7821a0b6700280e762eaa40bbb35
JayAtSeneca <jpchaudhari@myseneca.ca>
2023-04-30 16:49:40 -0400

    update added schema.ts file

--- src/graphql/schema/schema.ts
+++ src/graphql/schema/schema.ts
@@ -1,8 +1,19 @@
-import { GraphQLSchema } from "graphql";
-import query from "./resolvers/query/query";
+import { gql, makeExecutableSchema } from 'apollo-server';
+import { GraphQLSchema, printSchema } from 'graphql';
+import mutationType from '@src/graphql/schema/typedefs/MutationType';
+import queryType from '@src/graphql/schema/typedefs/QueryType';
+import resolvers from '@src/graphql/schema/resolvers/resolvers';
 
-const schema = new GraphQLSchema({
-    query,
+const schema = makeExecutableSchema({
+  typeDefs: gql(
+    printSchema(
+      new GraphQLSchema({
+        query: queryType,
+        mutation: mutationType,
+      })
+    )
+  ),
+  resolvers,
 });
 
 export default schema;
